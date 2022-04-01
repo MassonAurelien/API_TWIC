48c00b76670016eab1055da1bad13136bb5d5b67
Aurélien MASSON <aurelien.masson@reseau.eseo.fr>
2022-04-01 16:37:03 +0200

    Add Put and Delete Method in Controller

--- src/main/java/com/controller/VilleController.java
+++ src/main/java/com/controller/VilleController.java
@@ -3,8 +3,11 @@ package com.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,15 +28,6 @@ public class VilleController {
 		this.villeRepo = villerepo;
 	}
 	
-	//Fonction pour enregistrer dans la BDD
-	@PostMapping("AjouterVille")
-	public void post(
-			@RequestBody VilleModel ville
-			) {
-		this.villeRepo.save(new Ville(ville));
-		
-	}
-	
 	//Fonction pour récupérer le contenu de la BDD
 	@GetMapping("ville")
 	public List<Ville> get(@RequestParam(required  = false, value="codePostal") String codePostal) {
@@ -43,6 +37,41 @@ public class VilleController {
 		else {
 			return this.villeRepo.findAllByCodePostal(codePostal);
 		}
+	}
+	
+	//Fonction pour enregistrer dans la BDD
+	@PostMapping("AjouterVille")
+	public String add(
+			@RequestBody VilleModel ville
+			){
+		this.villeRepo.save(new Ville(ville));
+		return "Ville ajoutée";
+	}
+	
+	//Fonction pour delete dans la BDD
+	@DeleteMapping("SupprimerVille/{codeINSEE}")
+	public String delete(
+			@PathVariable String codeINSEE
+			){
+		this.villeRepo.deleteByCodeINSEE(codeINSEE);
+		return "Suppression effectuée";
+	}
+	
+	//Fonction pour update dans la BDD
+	@PutMapping("UpdateVille/{codeINSEE}")
+	public String update(
+			@RequestBody VilleModel ville,
+			@PathVariable String codeINSEE
+			) {
+		Ville villeBDD = this.villeRepo.getByCodeINSEE(codeINSEE);
+		villeBDD.setCommune(ville.getCommune());
+		villeBDD.setCodePostal(ville.getCodePostal());
+		villeBDD.setLibelleAcheminement(ville.getLibelleAcheminement());
+		villeBDD.setLigne5(ville.getLigne5());
+		villeBDD.setLatitude(ville.getLatitude());
+		villeBDD.setLongitude(ville.getLongitude());
+		this.villeRepo.save(villeBDD);
+		return "Update effectuée";
 	}
 
 }
--- src/main/java/com/repository/VilleRepo.java
+++ src/main/java/com/repository/VilleRepo.java
@@ -5,11 +5,17 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.entity.Ville;
+import com.model.VilleModel;
 
 @Repository
 public interface VilleRepo extends JpaRepository<Ville, String>, JpaSpecificationExecutor<Ville>{
 	List<Ville> findAllByCodePostal(String codePostal);
     List<Ville> findAll();
+    Ville getByCodeINSEE(String codeINSEE);
+    @Transactional
+    void deleteByCodeINSEE(String codeINSEE);
+	void saveAndFlush(VilleModel ville);
 }
